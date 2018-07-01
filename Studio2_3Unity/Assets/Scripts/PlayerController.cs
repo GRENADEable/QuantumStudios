3bf5b72931ee994b349ad09f2671ecc7c79aaf5e
Ajay Rawat <a.rawat@saedubai.com>
2018-07-01 11:06:33 +0400

    Added Player Controller and Basic Player and Arena

---
+++ Studio2_3Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float MoveSpeed;
+	private Rigidbody MyRB;
+	private Vector3 MovementInput;
+	private Vector3 MovementVelocity;
+
+	void Start () 
+	{
+		MyRB = GetComponent<Rigidbody>();
+	}
+	
+	
+	void Update () 
+	{
+		MovementInput = new Vector3 (Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+		MovementVelocity = MovementInput * MoveSpeed;
+	}
+	
+	void FixedUpdate()
+	{
+		MyRB.velocity = MovementVelocity;
+	}
+}
