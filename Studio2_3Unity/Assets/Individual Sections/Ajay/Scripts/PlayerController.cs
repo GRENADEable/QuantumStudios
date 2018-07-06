78804174934d01a749c18fcad1e2ead2d9b50da0
Ajay Rawat <a.rawat@saedubai.com>
2018-07-06 16:19:50 +0400

    Added the slowing effect for the whirlpool

--- Studio2_3Unity/Assets/Individual Sections/Ajay/Scripts/PlayerController.cs
+++ Studio2_3Unity/Assets/Individual Sections/Ajay/Scripts/PlayerController.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-
+	#region Public Variables
 	public float MoveSpeed;
+	#endregion
+
+	#region Private Variables
 	private Rigidbody MyRB;
 	private Vector3 MovementInput;
 	private Vector3 MovementVelocity;
-
+	#endregion 
+	 
 	void Start () 
 	{
 		MyRB = GetComponent<Rigidbody>();
@@ -18,11 +22,26 @@ public class PlayerController : MonoBehaviour {
 	void Update () 
 	{
 		MovementInput = new Vector3 (Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-		MovementVelocity = MovementInput * MoveSpeed;
+		MovementVelocity = MovementInput * MoveSpeed ;
 	}
 	
 	void FixedUpdate()
 	{
 		MyRB.velocity = MovementVelocity;
 	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "Whirlpool")
+		{		
+			MoveSpeed = 1;
+		}
+		
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+			MoveSpeed = 5;
+	}
 }
+
