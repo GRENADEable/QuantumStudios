eda42bbc2af17d579657ccead4aa11aaa61d9dc7
Khatim <kahtim.dar@gmail.com>
2018-07-07 01:21:25 +0400

    Username and Password Check works on local database.

---
+++ Studio2_3Unity/Assets/Individual Sections/Khatim/Scripts/Login/LoginScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginScript : MonoBehaviour
+{
+
+    #region Public Variables
+    public string inputUser;
+    public string inputPassword;
+    #endregion
+
+    #region Private Variables
+    private string userURL = "http://localhost/unity_login_system/usernamelogin.php";
+    #endregion
+
+    #region Callbacks
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            StartCoroutine(DBLogin(inputUser, inputPassword));
+    }
+    #endregion
+
+    #region My Functions
+    IEnumerator DBLogin(string playerName, string password)
+    {
+        WWWForm loginform = new WWWForm();
+        loginform.AddField("playerUsername", playerName);
+        loginform.AddField("playerPassword", password);
+        WWW dbLink = new WWW(userURL, loginform);
+
+        yield return dbLink;
+        Debug.Log(dbLink.text);
+    }
+    #endregion
+}
