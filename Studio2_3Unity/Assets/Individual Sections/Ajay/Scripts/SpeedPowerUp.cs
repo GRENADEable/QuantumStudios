4ad988e0fbbf784690df5dbc017b7974f6159edb
Ajay Rawat <a.rawat@saedubai.com>
2018-07-06 18:05:33 +0400

    Added the SpeedPowerUp

---
+++ Studio2_3Unity/Assets/Individual Sections/Ajay/Scripts/SpeedPowerUp.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedPowerUp : MonoBehaviour {
+
+	public GameObject PickUpFX;
+
+	public float Duration = 3f;
+	public float Multiplier = 2f;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			StartCoroutine(PickUp(other));
+		}
+	}
+
+	IEnumerator PickUp(Collider player)
+	{
+		Instantiate(PickUpFX, transform.position, transform.rotation);
+		
+		PlayerController ply = player.GetComponent<PlayerController>();
+		ply.MoveSpeed *= Multiplier;
+
+		GetComponent<MeshRenderer>().enabled = false;
+		GetComponent<Collider>().enabled = false;
+		
+		yield return new WaitForSeconds(Duration);
+		
+		ply.MoveSpeed /= Multiplier;
+		Destroy (gameObject);
+	}
+	void Start () 
+	{
+		
+	}
+	
+	
+	void Update () 
+	{
+		
+	}
+}
