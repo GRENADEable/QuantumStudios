ab439f097116f194456ea6e6cf628a29c0dcb457
Shariq <shariq.anwar677@gmail.com>
2018-07-13 18:09:37 +0400

    Added functionality to the UI

---
+++ Studio2_3Unity/Assets/Individual Sections/Shariq/Scripts/SceneManage.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManage : MonoBehaviour {
+	public GameObject MainMenuObject;
+	public GameObject ShopObject;
+	// Use this for initialization
+	void Awake () {
+
+		MainMenuObject.SetActive(true);
+		ShopObject.SetActive(false);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlaySinglePlayer()
+	{
+		SceneManager.LoadScene("Level");
+	}
+
+	public void Shop()
+	{
+		MainMenuObject.SetActive(false);
+		ShopObject.SetActive(true);
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
