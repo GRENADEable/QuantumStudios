d201b1daa14262654b9ee6156503c09dc332ea1d
Ajay Rawat <a.rawat@saedubai.com>
2018-07-15 09:34:26 +0400

    Added the CameraFollow

---
+++ Studio2_3Unity/Assets/Individual Sections/Ajay/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject Player;
+
+	private Vector3 Offset;
+
+	void Start () 
+	{
+		Offset = transform.position - Player.transform.position;
+	}
+	
+	void Update () 
+	{
+		
+	}
+
+	void LateUpdate()
+	{
+		transform.position = Player.transform.position + Offset;
+	}
+}
