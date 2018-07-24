020f1f812e69b722be1a62e80621e5e035bb16f5
Khatim <kahtim.dar@gmail.com>
2018-07-24 13:07:22 +0400

    Added Advertisement in the game.

---
+++ Studio2_3Unity/Assets/Individual Sections/Khatim/Scripts/Ads/StartAdvert.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class StartAdvert : MonoBehaviour
+{
+    #region Public Variables
+    #endregion
+
+    #region Private Variables
+    private BannerView bannerView;
+    #endregion
+
+    #region Callbacks
+    void Start()
+    {
+#if UNITY_ANDROID
+        string appId = "ca-app-pub-3940256099942544~3347511713";
+#else
+            string appId = "unexpected_platform";
+#endif
+
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(appId);
+        this.RequestBanner();
+    }
+    #endregion
+
+    #region My Functions
+    private void RequestBanner()
+    {
+#if UNITY_ANDROID
+        string adUnitId = "ca-app-pub-6818619256470654/4071939700";
+#else
+            string adUnitId = "unexpected_platform";
+#endif
+
+        // Create a 320x50 banner at the top of the screen.
+        bannerView = new BannerView(adUnitId, AdSize.SmartBanner, AdPosition.Top);
+
+        // Called when an ad request has successfully loaded.
+        bannerView.OnAdLoaded += HandleOnAdLoaded;
+        // Called when an ad request failed to load.
+        bannerView.OnAdFailedToLoad += HandleOnAdFailedToLoad;
+        // Called when an ad is clicked.
+        bannerView.OnAdOpening += HandleOnAdOpened;
+        // Called when the user returned from the app after an ad click.
+        bannerView.OnAdClosed += HandleOnAdClosed;
+        // Called when the ad click caused the user to leave the application.
+        bannerView.OnAdLeavingApplication += HandleOnAdLeavingApplication;
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+
+        // Load the banner with the request.
+        bannerView.LoadAd(request);
+        bannerView.Destroy();
+    }
+    public void HandleOnAdLoaded(object sender, EventArgs args)
+    {
+        Debug.LogWarning("HandleAdLoaded event received");
+    }
+
+    public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        Debug.LogWarning("Banner failed to load: " + args.Message);
+        // Handle the ad failed to load event.    
+    }
+
+    public void HandleOnAdOpened(object sender, EventArgs args)
+    {
+        Debug.LogWarning("HandleAdOpened event received");
+    }
+
+    public void HandleOnAdClosed(object sender, EventArgs args)
+    {
+        Debug.LogWarning("HandleAdClosed event received");
+    }
+
+    public void HandleOnAdLeavingApplication(object sender, EventArgs args)
+    {
+        Debug.LogWarning("HandleAdLeavingApplication event received");
+    }
+    #endregion
+}
