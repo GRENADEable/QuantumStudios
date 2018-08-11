92b4e26ac5613c7bfb59dcf268741d8ba79b6187
Ajay Rawat <a.rawat@saedubai.com>
2018-08-11 13:28:01 +0400

    Added a SceneManagement script to be edited by the Game Designer

---
+++ Studio2_3Unity/Assets/Individual Sections/Ajay/Scripts/CameraFollowSinglePlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowSinglePlayer : MonoBehaviour {
+
+	#region Private Variables
+	[SerializeField]
+	private GameObject Player;
+	[SerializeField]
+	private Vector3 Offset;
+	#endregion
+	
+	void Awake()
+	{
+		Player = GameObject.FindGameObjectWithTag("Player");
+	}
+	void Start () 
+	{
+		Offset = transform.position - Player.transform.position;
+	}	
+	void LateUpdate () 
+	{
+		transform.position = Player.transform.position + Offset;
+	}
+}
---
+++ Studio2_3Unity/Assets/Individual Sections/Ajay/Scripts/SceneManagement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour {
+
+	
+	public void SinglePlayer () 
+	{
+		SceneManager.LoadScene("AjayScene");
+	}
+	
+	public void Quit()
+	{
+		Application.Quit();
+		Debug.Log("Quit");
+	}
+
+	public void MainMenu()
+	{
+		SceneManager.LoadScene("LobbyScene");
+	}
+	
+	void Update () 
+	{
+		
+	}
+}
