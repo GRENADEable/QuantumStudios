53b499583915a71bd4ca0e2185df36c8683a54a6
Ajay Rawat <a.rawat@saedubai.com>
2018-08-16 15:57:19 +0400

    Added the Whirlpool Vortex as well as the PowerUp rotation (Both to be updated)

---
+++ Studio2_3Unity/Assets/Individual Sections/Ajay/Scripts/PowerUpRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpRotation : MonoBehaviour {
+
+	
+	void Start () 
+	{
+		
+	}
+	
+	
+	void Update () 
+	{
+		transform.Rotate (new Vector3(0, Time.deltaTime * 50, 0));
+	}
+}
---
+++ Studio2_3Unity/Assets/Individual Sections/Ajay/Scripts/WhirlpoolVortex.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhirlpoolVortex : MonoBehaviour {
+
+	public float pullSpeed;
+	private GameObject pulledObj;
+	//private Rigidbody whirlpoolrb;
+
+	void Start () 
+	{
+		//Rigidbody whirlpoolrb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	
+	void Update () 
+	{
+		
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			pulledObj = other.gameObject;
+			pulledObj.transform.position = Vector3.MoveTowards(pulledObj.transform.position, this.transform.position, pullSpeed * Time.deltaTime);
+		}
+	}
+}
