04723ad1fd9ba1d743976f26eeb62d5d9d0e7c60
Khatim <kahtim.dar@gmail.com>
2018-08-18 16:08:45 +0400

    Made the speed powerup spawner work online

--- Studio2_3Unity/Assets/Individual Sections/Ajay/Scripts/PowerUpSpawner.cs
+++ Studio2_3Unity/Assets/Individual Sections/Ajay/Scripts/PowerUpSpawner.cs
@@ -2,52 +2,100 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerUpSpawner : MonoBehaviour {
+public class PowerUpSpawner : Photon.MonoBehaviour
+{
+    #region Public Variables
+    public float maxTime;
+    public float timer = 0;
+    #endregion
 
-	public float maxTime;
-	public float timer  = 0;
-	
-	[SerializeField]
-	private GameObject[] powerupSpawnLocation;
-	[SerializeField]
-	private int index;
-	[SerializeField]
-	private GameObject powerUp;
-	[SerializeField]
-	private List<GameObject> powerPickUp;
-	
-	void Awake()
-	{
-		timer = maxTime;
-		powerupSpawnLocation = GameObject.FindGameObjectsWithTag("SpeedPowerSpawn");
-		for (int i = 0; i < powerupSpawnLocation.Length; i++)
-		{
-			GameObject obj = Instantiate(powerUp, powerupSpawnLocation[i].transform.position, Quaternion.identity);
-			powerUp.SetActive(false);
-			powerPickUp.Add(obj);
-		}
-	}
-	void Start () 
-	{	
+    #region Private Variables
+    [SerializeField]
+    private GameObject[] powerupSpawnLocation;
+    [SerializeField]
+    private string index;
+    [SerializeField]
+    private int offlineIndex;
+    [SerializeField]
+    private GameObject powerUp;
+    [SerializeField]
+    private List<GameObject> powerPickUp;
+    #endregion
 
-	}
-	
-	
-	void FixedUpdate () 
-	{
-		index = Random.Range(0, powerPickUp.Count);
-		timer -= Time.deltaTime;
-		if(timer <= 0)
-		{
-			Spawner();
-			timer = maxTime;
-		}
-	}
-	void Spawner()
-	{
-		if (!powerPickUp[index].activeInHierarchy)
-		{
-			powerPickUp[index].SetActive(true);
-		}
-	}
+    #region Unity Callbacks
+    void Awake()
+    {
+        if (PhotonNetwork.connected && PhotonNetwork.isMasterClient)
+        {
+            timer = maxTime;
+            powerupSpawnLocation = GameObject.FindGameObjectsWithTag("SpeedPowerSpawn");
+
+            for (int i = 0; i < powerupSpawnLocation.Length; i++)
+            {
+                GameObject obj = PhotonNetwork.Instantiate(powerUp.name, powerupSpawnLocation[i].transform.position, Quaternion.identity, 0);
+                powerUp.SetActive(false);
+                powerPickUp.Add(obj);
+            }
+        }
+        else
+        {
+            timer = maxTime;
+            powerupSpawnLocation = GameObject.FindGameObjectsWithTag("SpeedPowerSpawn");
+            for (int i = 0; i < powerupSpawnLocation.Length; i++)
+            {
+                GameObject obj = Instantiate(powerUp, powerupSpawnLocation[i].transform.position, Quaternion.identity);
+                powerUp.SetActive(false);
+                powerPickUp.Add(obj);
+            }
+        }
+    }
+    void FixedUpdate()
+    {
+        if (PhotonNetwork.isMasterClient && PhotonNetwork.connected)
+        {
+            index = Random.Range(0, powerPickUp.Count).ToString();
+            timer -= Time.deltaTime;
+            if (timer <= 0f)
+            {
+                this.photonView.RPC("Spawner", PhotonTargets.All, index);
+                timer = maxTime;
+            }
+        }
+        else
+        {
+            offlineIndex = Random.Range(0, powerPickUp.Count);
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                OfflineSpawner();
+                timer = maxTime;
+            }
+        }
+    }
+    #endregion
+
+    #region My Functions
+    [PunRPC]
+    void Spawner(string intConvert)
+    {
+        Debug.LogWarning("Spawning Health");
+        int convertIndex = int.Parse(intConvert);
+        for (int i = 0; i < powerPickUp.Count; i++)
+        {
+            if (!powerPickUp[i].activeInHierarchy)
+            {
+                powerPickUp[convertIndex].SetActive(true);
+                break;
+            }
+        }
+    }
+
+    void OfflineSpawner()
+    {
+        if (!powerPickUp[offlineIndex].activeInHierarchy)
+        {
+            powerPickUp[offlineIndex].SetActive(true);
+        }
+    }
+    #endregion
 }
