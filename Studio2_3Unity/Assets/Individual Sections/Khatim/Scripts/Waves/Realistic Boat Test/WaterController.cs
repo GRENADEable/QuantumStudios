3c95d6ac7a41f3a033082b098b1877b541c6b7a7
Khatim <kahtim.dar@gmail.com>
2018-08-21 11:38:13 +0400

    Added Buoyancy in the objects

---
+++ Studio2_3Unity/Assets/Individual Sections/Khatim/Scripts/Waves/Realistic Boat Test/WaterController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterController : MonoBehaviour
+{
+    #region Public Variables
+    public static WaterController current;
+
+    public bool isMoving;
+
+    //Wave height and speed
+    public float scale = 0.1f;
+    public float speed = 1.0f;
+    //The width between the waves
+    public float waveDistance = 1f;
+    //Noise parameters
+    public float noiseStrength = 1f;
+    public float noiseWalk = 1f;
+    #endregion
+
+    #region Unity Calbacks
+    void Start()
+    {
+        current = this;
+    }
+    #endregion
+
+    #region My Functions
+    //Get the y coordinate from whatever wavetype we are using
+    public float GetWaveYPos(Vector3 position, float timeSinceStart)
+    {
+        //if (isMoving)
+        //{
+        //return WaveTypes.SinXWave(position, speed, scale, waveDistance, noiseStrength, noiseWalk, timeSinceStart);
+        //}
+        //else
+        //{
+        //return 0f;
+        //}
+
+        return 0f;
+    }
+
+    //Find the distance from a vertice to water
+    //Make sure the position is in global coordinates
+    //Positive if above water
+    //Negative if below water
+    public float DistanceToWater(Vector3 position, float timeSinceStart)
+    {
+        float waterHeight = GetWaveYPos(position, timeSinceStart);
+
+        float distanceToWater = position.y - waterHeight;
+
+        return distanceToWater;
+    }
+    #endregion
+}
