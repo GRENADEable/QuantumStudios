c8f6e75de162427ef657ac2ced833ae0b5426ff9
Ajay Rawat <a.rawat@saedubai.com>
2018-09-05 11:15:26 +0400

    Created the TutorialScene for the game

---
+++ Studio2_3Unity/Assets/Canvas_Destroy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canvas_Destroy : MonoBehaviour {
+
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag =="Player")
+		{
+			Destroy(this.gameObject, 5f);
+		}
+	}
+		
+	}
+
