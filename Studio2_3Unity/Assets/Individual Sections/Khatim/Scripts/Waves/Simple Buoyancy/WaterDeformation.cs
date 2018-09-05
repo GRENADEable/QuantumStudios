44bb6eac75bf6fb17ca5f5725d575f38be50aeef
Khatim <kahtim.dar@gmail.com>
2018-09-05 11:18:34 +0400

    Changes in the main menu again

--- Studio2_3Unity/Assets/Individual Sections/Khatim/Scripts/Waves/Simple Buoyancy/WaterDeformation.cs
+++ Studio2_3Unity/Assets/Individual Sections/Khatim/Scripts/Waves/Simple Buoyancy/WaterDeformation.cs
@@ -6,14 +6,8 @@ public class WaterDeformation : MonoBehaviour
     #region Public Variables
     public static Mesh mesh;
     public static Transform water;
-    /*public float deformAmount = 1;
-    public float scale = 2.5f;
-    public float speed = 1;*/
     #endregion
 
-    #region Private Variables
-    //private Vector2 time = Vector2.zero;
-    #endregion
 
     #region Unity Calbacks
     void Start()
@@ -21,34 +15,14 @@ public class WaterDeformation : MonoBehaviour
         water = transform;
         mesh = GetComponent<MeshFilter>().mesh;
     }
-
-    void Update()
-    {
-
-    }
     void FixedUpdate()
     {
-        //time = new Vector2(Time.time, Time.time) * speed;
-
         Vector3[] vertices = mesh.vertices;
-
-        /*for (int i = 0; i < vertices.Length; i++)
-        {
-            vertices[i] = Deform(vertices[i]);
-        }*/
 
         mesh.vertices = vertices;
         mesh.RecalculateNormals();
 
         GetComponent<MeshFilter>().mesh = mesh;
     }
-    #endregion
-
-    #region My Functions
-    /*Vector3 Deform(Vector3 v)
-    {
-        v.y = Mathf.PerlinNoise(v.x / scale + time.x, v.z / scale + time.y) * deformAmount;
-        return v;
-    }*/
     #endregion
 }
